5c4c14318e690baf94b0fdeaf10c821e0a59287d
ashok285 <akunam@italentdigital.com>
2018-11-06 22:44:56 +0530

    Master refresh

--- 03-11-2018/HybernateExample/src/main/java/com/hibernate/dao/DatabaseConnectionDao.java
+++
@@ -1,6 +0,0 @@
-package com.hibernate.dao;
-
-public interface DatabaseConnectionDao 
-{
-	public void createTable();
-}
--- 06-11-2018/Italent2BoardCreation/src/main/java/com/italent2/dao/DatabaseConnectionDao.java
+++
@@ -1,9 +0,0 @@
-package com.italent2.dao;
-
-import com.italent2.model.Messages;
-
-public interface DatabaseConnectionDao 
-{
-	public void createTable();
-	public void insertIntoTable(Messages message);
-}
