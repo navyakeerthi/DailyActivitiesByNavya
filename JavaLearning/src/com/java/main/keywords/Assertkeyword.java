008fc6d835ce67e1e25d3f7ba058b152fd2c3a13
“navyakeerthi” <navya4343@gmail.com>
2018-11-23 18:07:11 +0530

    23-11-2018

---
+++ JavaLearning/src/com/java/main/keywords/Assertkeyword.java
@@ -0,0 +1,12 @@
+package com.java.main.keywords;
+
+public class Assertkeyword 
+{
+	public static void main( String args[] ) 
+	{ 
+		int value = 25; 
+		assert value >= 20 : " Underweight"; 
+		System.out.println("value is "+value); 
+		//assertFalse("hai",false);
+	} 
+}
---
+++ JavaLearning/src/com/java/main/keywords/InstanceOf.java
@@ -0,0 +1,10 @@
+package com.java.main.keywords;
+
+public class InstanceOf 
+{
+	public static void main(String args[])
+	{
+		InstanceOf io=new InstanceOf();
+		System.out.println(io instanceof InstanceOf);
+	}
+}
